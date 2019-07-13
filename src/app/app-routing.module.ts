ffc6581a222e40ee22d021e218f732cf0c0fc40b
Elliot Bobillet <elliot.bobillet@hotmail.fr>
2019-07-13 16:05:11 +0200

    maj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { CvComponent } from './cv/cv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'accueil',
+    component: AcceuilComponent
+  },
+  {
+    path: '',
+    redirectTo: 'accueil',
+    pathMatch: 'full'
+  },
+  {
+    path: 'curriculum',
+    component: CvComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CvComponent } from './cv/cv.component';
+import { ProfilComponent } from './profil/profil.component';
+import { Projet1Component } from './projet1/projet1.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+
+const appRoutes: Routes =
+[
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CvComponent,
+    ProfilComponent,
+    Projet1Component,
+    AcceuilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
